eb807052005469dd378995e530f0897c9230260e
maruthicreat <maruthicreat@gmail.com>
2017-12-24 19:05:35 +0530

    login verify

--- app/src/main/java/com/example/maruthiraja/smartshopping/ShopkeeperLog.java
+++ app/src/main/java/com/example/maruthiraja/smartshopping/ShopkeeperLog.java
@@ -3,6 +3,8 @@ package com.example.maruthiraja.smartshopping;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class ShopkeeperLog extends Activity{
@@ -10,5 +12,17 @@ public class ShopkeeperLog extends Activity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shopkeeperloginpage);
+    }
+
+    public void testpass(View v){
+        EditText useredit = (EditText) findViewById(R.id.usertext);
+        EditText passedit = (EditText) findViewById(R.id.passtext);
+        String usertext = String.valueOf(useredit.getText());
+        String passtext = String.valueOf(passedit.getText());
+        System.out.println("user :"+usertext+" passtext :" + passtext);
+        if(usertext.equals(passtext))
+        {
+            System.out.println("success");
+        }
     }
 }
